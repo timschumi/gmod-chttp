d9964a325d959e47041d381130211aa41b72f378
Tim Schumacher <timschumi@gmx.de>
2019-09-18 18:04:57 +0200

    Add a simple message printer and stub out HTTP()

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,57 @@
 
 using namespace GarrysMod;
 
-int Example(lua_State *state) {
-	LUA->PushString("Hello from Example()!");
+static void printMessage(lua_State *state, const char* message) {
+	// Push global table to the stack to work on it
+	LUA->PushSpecial(Lua::SPECIAL_GLOB);
 
-	return 1;
+	// Gets the print function and stores it at the top of the stack (top = -1)
+	LUA->GetField(-1, "print");
+
+	// Pushes the argument to the stack
+	LUA->PushString(message);
+
+	// Calls the function (arg1 = number of arguments, arg2 = number of return values).
+	// The function is the top of the stack after arguments have been popped.
+	// In this case, we are calling with 1 argument (the message) and no return values (because it's `print`, duh!)
+	LUA->Call(1, 0);
+
+	// Pops the last value from the stack (the global table?)
+	LUA->Pop();
+}
+
+/*
+ * See https://wiki.garrysmod.com/page/Global/HTTP for documentation.
+ * The function takes a single table argument, based off the HTTPRequest structure.
+ * It returns a boolean whether the request failed or succeeded.
+ */
+int CHTTP(lua_State *state) {
+	printMessage(state, "Called HTTP()! STUB!");
+
+	LUA->PushBool(true); // Push result to the stack
+	return 1; // We are returning a single value
 }
 
 GMOD_MODULE_OPEN() {
+	printMessage(state, "Loading CHTTP module.");
+
+	// We are working on the global table today
 	LUA->PushSpecial(Lua::SPECIAL_GLOB);
-	LUA->PushString("Example");
-	LUA->PushCFunction(Example);
+
+	// Push the function mapping (first is the key/function name,
+	// second is the value/actual function)
+	LUA->PushString("HTTP");
+	LUA->PushCFunction(CHTTP);
+
+	// SetTable takes the item at the top of the stack (value) and
+	// the second item from the top (key) and adds them to the table
+	// at the stack offset mentioned in the parameter (again, -1 is the top)
 	LUA->SetTable(-3);
+
+	// Pop the global table from the stack again
+	LUA->Pop();
+
+	printMessage(state, "Finished loading CHTTP module.");
 
 	return 0;
 }
