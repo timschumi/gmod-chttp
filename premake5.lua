81b4d8edfe2abded98b45dedba3703e74ec104c3
Tim Schumacher <timschumi@gmx.de>
2019-09-28 12:51:28 +0200

    Resolve some premake warnings
    I'm not sure how I feel about the debug symbols in release builds,
    but whatever...

--- premake5.lua
+++ premake5.lua
@@ -15,12 +15,12 @@ solution "chttp"
 	--
 	-- Statically link the C-Runtime to reduce dependencies needed to run our module
 	--
-	flags "StaticRuntime"
+	staticruntime "On"
 
 	configurations { "Release" }
 
 	configuration "Release"
-		flags		"symbols"	-- Generate debugging information
+		symbols		"On"		-- Generate debugging information
 		optimize	"On"		-- Optimize the build output for size and speed
 
 	project "chttp"
