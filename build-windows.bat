81354b9844ce6dd256658187ca3c6a8c29a54187
Tim Schumacher <timschumi@gmx.de>
2019-10-04 18:46:36 +0200

    build-windows: Add support for Visual Studio 2019

--- build-windows.bat
+++ build-windows.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Building Module.
-echo Dependencies: GIT, PREMAKE5, VISUAL STUDIO 2015/2013.
+echo Dependencies: GIT, PREMAKE5, VISUAL STUDIO 2019/2015/2013.
 echo.
 
 :: Hacky way to ensure we've got a git repository & submodules at our disposal
@@ -10,7 +10,10 @@ git submodule update --init --recursive
 echo.
 
 :: Fire up a Visual Studio command-line environment and generate our solution
-IF DEFINED VS140COMNTOOLS (
+IF DEFINED VS160COMNTOOLS (
+	echo Preparing for build with Visual Studio 2019.
+	premake5 vs2019
+) ELSE IF DEFINED VS140COMNTOOLS (
 	echo Preparing for build with Visual Studio 2015.
 	premake5 vs2015
 	CALL "%VS140COMNTOOLS%VSDevCmd.bat"
@@ -20,6 +23,9 @@ IF DEFINED VS140COMNTOOLS (
 	CALL "%VS120COMNTOOLS%VSDevCmd.bat"
 ) ELSE (
 	echo Failed to detect Visual Studio installation.
+	echo If you are using Visual Studio 2017 or newer,
+	echo you have to launch this script through
+	echo the "Developer Command Prompt".
 	timeout 5
 	exit /B 1
 )
