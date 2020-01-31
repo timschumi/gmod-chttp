56e4b7acb8a4ee609a0289e9f4f8dd8d19d98ef3
Tim Schumacher <timschumi@gmx.de>
2020-01-31 03:22:11 +0100

    Rework the premake file

--- premake5.lua
+++ premake5.lua
@@ -1,47 +1,42 @@
-local target_suffixes_86 = {
-	bsd       = "_linux",
-	linux     = "_linux",
-	solaris   = "_linux",
-	windows   = "_win32",
-	macosx    = "_osx"  ,
-}
-
-local target_suffixes_64 = {
-	bsd       = "_linux64",
-	linux     = "_linux64",
-	solaris   = "_linux64",
-	windows   = "_win64",
-	macosx    = "_osx64",
-}
-
 solution "chttp"
 	language		"C++"
 	location		"project"
 
-	--
-	-- Statically link the C-Runtime to reduce dependencies needed to run our module
-	--
-	staticruntime "On"
+	staticruntime	"On"		-- Statically link the C-Runtime to reduce dependencies needed to run our module
+	optimize	"On"		-- Optimize the build output for size and speed
 
 	configurations { "Debug", "Release" }
-	platforms { "x86", "x64" }
+	platforms { "linux64", "linux32", "win64", "win32" }
 
 	configuration "Debug"
-		defines { "DEBUG_BUILD" }
 		symbols		"On"		-- Generate debugging information
-		optimize	"On"		-- Optimize the build output for size and speed
 
-	configuration "Release"
-		defines { "RELEASE_BUILD" }
-		optimize	"On"		-- Optimize the build output for size and speed
+	filter "platforms:win*"
+		system "windows"
+		defines { "WINDOWS_BUILD" }
+		libdirs { "curl/lib" }
 
-	filter "platforms:x86"
+	filter "platforms:win32"
 		architecture "x86"
-		targetsuffix ( target_suffixes_86[os.target()] )
+		targetsuffix "_win32"
+		links { "libcurl" }
 
-	filter "platforms:x64"
+	filter "platforms:win64"
 		architecture "x86_64"
-		targetsuffix ( target_suffixes_64[os.target()] )
+		targetsuffix "_win64"
+		links { "libcurl-x64" }
+
+	filter "platforms:linux*"
+		system "linux"
+		links { "curl" }
+
+	filter "platforms:linux32"
+		architecture "x86"
+		targetsuffix "_linux"
+
+	filter "platforms:linux64"
+		architecture "x86_64"
+		targetsuffix "_linux64"
 
 	project "chttp"
 		kind	"SharedLib"
@@ -49,13 +44,3 @@ solution "chttp"
 		targetextension ".dll"
 		includedirs { "curl/include/", "gmod-module-base/include/" }
 		files { "src/*.cpp", "src/*.h", "src/"..os.target().."/*.cpp", "src/"..os.target().."/*.h" }
-		if os.target() == "windows" then
-		    defines { "WINDOWS_BUILD" }
-		    libdirs {"curl/lib"}
-			filter "platforms:x64"
-				links {"libcurl-x64"}
-			filter "platforms:x86"
-				links {"libcurl"}
-		else
-		    links {"curl"}
-		end
