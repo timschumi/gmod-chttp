322f5e28e53260602b07ca0a1ec2534de42f4146
Tim Schumacher <timschumi@gmx.de>
2020-07-30 20:34:20 +0200

    vagrant-win: Manually set grub installation path

--- vagrant-win.sh
+++ vagrant-win.sh
@@ -2,6 +2,9 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# HACK: Manually tell debian where to install grub
+debconf-set-selections <<< "grub-pc grub-pc/install_devices string /dev/vda"
+
 apt update && apt -y upgrade
 
 apt -y install build-essential cmake mingw-w64
