4147b30ae1bba9c71dd28018a4df1acff68106ca
Tim Schumacher <timschumi@gmx.de>
2020-08-18 12:42:53 +0200

    tests: Prefix functions with chttp_test_

--- tests/test.lua
+++ tests/test.lua
@@ -3,7 +3,7 @@ include("testlib.lua")
 
 require("chttp")
 
-assertEquals("CHTTP() should return nothing if no request table is given.", 0, select('#', CHTTP()))
+chttp_test_assertEquals("CHTTP() should return nothing if no request table is given.", 0, select('#', CHTTP()))
 
-assertEquals("CHTTP({}) should return a value.", 1, select('#', CHTTP({})))
-assertTrue("CHTTP({}) should return true.", CHTTP({}))
+chttp_test_assertEquals("CHTTP({}) should return a value.", 1, select('#', CHTTP({})))
+chttp_test_assertTrue("CHTTP({}) should return true.", CHTTP({}))
--- tests/testlib.lua
+++ tests/testlib.lua
@@ -1,51 +1,51 @@
-function info(msg)
+function chttp_test_info(msg)
     MsgC(Color(61, 245, 229), msg)
 end
 
-function success(msg)
+function chttp_test_success(msg)
     MsgC(Color(69, 255, 122), msg)
 end
 
-function failed(msg)
+function chttp_test_failed(msg)
     MsgC(Color(255, 84, 84), msg)
 end
 
-function assertEquals(message, expected, actual)
-    info("Test: " .. message .. "\n")
+function chttp_test_assertEquals(message, expected, actual)
+    chttp_test_info("Test: " .. message .. "\n")
 
     if expected == actual then
-        success("Successful.\n\n")
+        chttp_test_success("Successful.\n\n")
         return
     end
 
-    failed("expected: " .. expected .. "\n")
-    failed("actual: " .. actual .. "\n")
+    chttp_test_failed("expected: " .. expected .. "\n")
+    chttp_test_failed("actual: " .. actual .. "\n")
     MsgC("\n")
 end
 
-function assertTrue(message, actual)
-    info("Test: " .. message .. "\n")
+function chttp_test_assertTrue(message, actual)
+    chttp_test_info("Test: " .. message .. "\n")
 
     if actual then
-        success("Successful.\n\n")
+        chttp_test_success("Successful.\n\n")
         return
     end
 
-    failed("Failed.\n\n")
+    chttp_test_failed("Failed.\n\n")
 end
 
-function assertFalse(message, actual)
-    info("Test: " .. message .. "\n")
+function chttp_test_assertFalse(message, actual)
+    chttp_test_info("Test: " .. message .. "\n")
 
     if actual then
-        success("Successful.\n\n")
+        chttp_test_success("Successful.\n\n")
         return
     end
 
-    failed("Failed.\n\n")
+    chttp_test_failed("Failed.\n\n")
 end
 
-function dumpResponse(code, body, headers)
+function chttp_test_dumpResponse(code, body, headers)
     print("Code: " .. code .."\n")
 
     print("Body:")
