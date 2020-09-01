b510197e44818bd0f6ca4a48506d4198c51d62a8
Tim Schumacher <timschumi@gmx.de>
2020-09-02 01:25:18 +0200

    Put the actual build commands into a seperate file

--- dist.sh
+++ dist.sh
@@ -5,45 +5,13 @@ vagrant up
 
 # Empty dist folder
 rm -rf dist
-mkdir -p dist
 
 # Build the different configurations
-vagrant ssh x86 -c "rm -rf chttp-linux-x86-dbg && mkdir -p chttp-linux-x86-dbg && cd chttp-linux-x86-dbg &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux.cmake -DCMAKE_BUILD_TYPE=Debug /vagrant &&
-                    make &&
-                    cp gmsv_chttp_linux.dll /vagrant/dist/gmsv_chttp_linux-dbg.dll"
-
-vagrant ssh x64 -c "rm -rf chttp-linux-x64-dbg && mkdir -p chttp-linux-x64-dbg && cd chttp-linux-x64-dbg &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux64.cmake -DCMAKE_BUILD_TYPE=Debug /vagrant &&
-                    make &&
-                    cp gmsv_chttp_linux64.dll /vagrant/dist/gmsv_chttp_linux64-dbg.dll"
-
-vagrant ssh x86 -c "rm -rf chttp-linux-x86-rel && mkdir -p chttp-linux-x86-rel && cd chttp-linux-x86-rel &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux.cmake -DCMAKE_BUILD_TYPE=Release /vagrant &&
-                    make &&
-                    cp gmsv_chttp_linux.dll /vagrant/dist/gmsv_chttp_linux.dll"
-
-vagrant ssh x64 -c "rm -rf chttp-linux-x64-rel && mkdir -p chttp-linux-x64-rel && cd chttp-linux-x64-rel &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux64.cmake -DCMAKE_BUILD_TYPE=Release /vagrant &&
-                    make &&
-                    cp gmsv_chttp_linux64.dll /vagrant/dist/gmsv_chttp_linux64.dll"
-
-vagrant ssh win -c "rm -rf chttp-windows-x86-dbg && mkdir -p chttp-windows-x86-dbg && cd chttp-windows-x86-dbg &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-win32.cmake -DCMAKE_BUILD_TYPE=Debug /vagrant &&
-                    make &&
-                    cp gmsv_chttp_win32.dll /vagrant/dist/gmsv_chttp_win32-dbg.dll"
-
-vagrant ssh win -c "rm -rf chttp-windows-x64-dbg && mkdir -p chttp-windows-x64-dbg && cd chttp-windows-x64-dbg &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-win64.cmake -DCMAKE_BUILD_TYPE=Debug /vagrant &&
-                    make &&
-                    cp gmsv_chttp_win64.dll /vagrant/dist/gmsv_chttp_win64-dbg.dll"
-
-vagrant ssh win -c "rm -rf chttp-windows-x86-rel && mkdir -p chttp-windows-x86-rel && cd chttp-windows-x86-rel &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-win32.cmake -DCMAKE_BUILD_TYPE=Release /vagrant &&
-                    make &&
-                    cp gmsv_chttp_win32.dll /vagrant/dist/gmsv_chttp_win32.dll"
-
-vagrant ssh win -c "rm -rf chttp-windows-x64-rel && mkdir -p chttp-windows-x64-rel && cd chttp-windows-x64-rel &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-win64.cmake -DCMAKE_BUILD_TYPE=Release /vagrant &&
-                    make &&
-                    cp gmsv_chttp_win64.dll /vagrant/dist/gmsv_chttp_win64.dll"
+vagrant ssh x86 -c "/vagrant/dist-build-single.sh linux Debug"
+vagrant ssh x64 -c "/vagrant/dist-build-single.sh linux64 Debug"
+vagrant ssh x86 -c "/vagrant/dist-build-single.sh linux Release"
+vagrant ssh x64 -c "/vagrant/dist-build-single.sh linux64 Release"
+vagrant ssh win -c "/vagrant/dist-build-single.sh win32 Debug"
+vagrant ssh win -c "/vagrant/dist-build-single.sh win64 Debug"
+vagrant ssh win -c "/vagrant/dist-build-single.sh win32 Release"
+vagrant ssh win -c "/vagrant/dist-build-single.sh win64 Release"
