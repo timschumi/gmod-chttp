e3ca52e6a62354a1d0104ab7c0621d323128b3ca
Tim Schumacher <timschumi@gmx.de>
2020-09-02 01:08:56 +0200

    Rename linux32 toolchain file to just linux

--- dist.sh
+++ dist.sh
@@ -9,7 +9,7 @@ mkdir -p dist
 
 # Build the different configurations
 vagrant ssh x86 -c "rm -rf chttp-linux-x86-dbg && mkdir -p chttp-linux-x86-dbg && cd chttp-linux-x86-dbg &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux32.cmake -DCMAKE_BUILD_TYPE=Debug /vagrant &&
+                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux.cmake -DCMAKE_BUILD_TYPE=Debug /vagrant &&
                     make &&
                     cp gmsv_chttp_linux.dll /vagrant/dist/gmsv_chttp_linux-dbg.dll"
 
@@ -19,7 +19,7 @@ vagrant ssh x64 -c "rm -rf chttp-linux-x64-dbg && mkdir -p chttp-linux-x64-dbg &
                     cp gmsv_chttp_linux64.dll /vagrant/dist/gmsv_chttp_linux64-dbg.dll"
 
 vagrant ssh x86 -c "rm -rf chttp-linux-x86-rel && mkdir -p chttp-linux-x86-rel && cd chttp-linux-x86-rel &&
-                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux32.cmake -DCMAKE_BUILD_TYPE=Release /vagrant &&
+                    cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-linux.cmake -DCMAKE_BUILD_TYPE=Release /vagrant &&
                     make &&
                     cp gmsv_chttp_linux.dll /vagrant/dist/gmsv_chttp_linux.dll"
 
