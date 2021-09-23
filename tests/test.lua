ee30a18c998409e155bef37a1f37cf22b9a039bd
Tim Schumacher <timschumi@gmx.de>
2021-09-23 11:47:37 +0200

    Remove useless tests

--- tests/test.lua
+++
@@ -1,9 +0,0 @@
--- Load utility functions
-include("testlib.lua")
-
-require("chttp")
-
-chttp_test_assertEquals("CHTTP() should return nothing if no request table is given.", 0, select('#', CHTTP()))
-
-chttp_test_assertEquals("CHTTP({}) should return a value.", 1, select('#', CHTTP({})))
-chttp_test_assertTrue("CHTTP({}) should return true.", CHTTP({}))
