c9af5f0e086d5da518863c33471b03559a1e9c2a
Tim Schumacher <timschumi@gmx.de>
2022-11-12 12:50:17 +0100

    Run tests on GitHub Actions

---
+++ tests/chttp.lua
@@ -0,0 +1,30 @@
+return {
+    groupName = "CHTTP",
+    cases = {
+        {
+            name = "CHTTP loads successfully",
+            func = function()
+                expect(require, "chttp").to.succeed()
+                expect(CHTTP).to.exist()
+            end
+        },
+        {
+            name = "HTTPS requests run successfully",
+            async = true,
+            timeout = 1,
+            func = function()
+                CHTTP({
+                    url = "https://example.com",
+                    success = function(code, body, headers)
+                        expect(code).to.equal(200)
+                        done()
+                    end,
+                    failed = function(err)
+                        error("HTTP request failed: " .. err)
+                        done()
+                    end
+                })
+            end
+        },
+    }
+}
